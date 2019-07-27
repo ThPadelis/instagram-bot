3f3fcfef950326a7c501db99ed55b33c756219f6
Padelis Theodosiou <33872406+ThPadelis@users.noreply.github.com>
2019-07-27 14:03:19 +0300

    login function

--- instaBot.py
+++ instaBot.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class InstagramBot:
@@ -6,3 +8,16 @@ class InstagramBot:
         self.username = username
         self.password = password
         self.driver = webdriver.Firefox()
+
+    def login(self):
+        driver = self.driver
+        driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
+        time.sleep(5)
+        email = driver.find_element_by_name("username")
+        password = driver.find_element_by_name("password")
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(5)
